6f58a5df609e5d39566326e7e85845608502f417
meetdesai10 <dmeet1008@gmail.com>
2024-04-16 12:43:03 +0530

    :

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "http://localhost:5173"],
     credentials: true,
   })
 );
